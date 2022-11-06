07d6b9661ffe35b8ffa8b84a083eb40e49a692b5
Yonghyun Kwon <91252737+Yonghyunkkk@users.noreply.github.com>
2022-11-06 15:53:05 +0800

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import Webcam from 'react-webcam'
+import axios from 'axios'
+const WebcamComponent = () => <Webcam />
+const videoConstraints = {
+  width: 400,
+  height: 400,
+  facingMode: 'user',
+}
+const Profile = () => {
+  const [picture, setPicture] = useState('')
+  const webcamRef = React.useRef(null)
+  const capture = React.useCallback(() => {
+    const pictureSrc = webcamRef.current.getScreenshot()
+    setPicture(pictureSrc)
+  })
+  return (
+    <div>
+      <h2 className="mb-5 text-center">
+        React Photo Capture using Webcam Examle
+      </h2>
+      <div>
+        {picture == '' ? (
+          <Webcam
+            audio={false}
+            height={400}
+            ref={webcamRef}
+            width={400}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={picture} />
+        )}
+      </div>
+      <div>
+        {picture != '' ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              setPicture()
+              console.log(picture)
+        
+              axios.get('http://localhost:8000/check')
+              .then(res => console.log(res))
+            }}
+            className="btn btn-primary"
+          >
+            Retake
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              capture()
+              axios.post('http://localhost:8000/server',{name:picture}).then(res => console.log(res))
+            }}
+            className="btn btn-danger"
+          >
+            Capture
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+export default Profile
